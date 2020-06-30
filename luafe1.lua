5bbdb8dd8e4a50b35c0795ae4c2b496c5308884b
cheeseybot <65479634+cheeseybot@users.noreply.github.com>
2020-06-30 18:42:55 +1000

    Create luafe1.lua

---
+++ luafe1.lua
@@ -0,0 +1,212 @@
+--[[--
+
+-- WARNING NOT ADMIN SCRIPT --
+
+Updated Script / Verifyed By CheeseDev
+Made By 942x824 / Head Staff Of Fox X & LightRed
+
+--]]--
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local SGOpal = Instance.new("TextButton")
+local SG = Instance.new("TextButton")
+local SynapseX = Instance.new("TextButton")
+local ESploit = Instance.new("TextButton")
+local TextLabel = Instance.new("TextLabel")
+local Close = Instance.new("TextButton")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local RevzAdmin = Instance.new("TextButton")
+local TextButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Main.Position = UDim2.new(0.313404024, 0, 0.225092247, 0)
+Main.Size = UDim2.new(0, 401, 0, 345)
+Main.Visible = false
+Main.Style = Enum.FrameStyle.RobloxRound
+Main.Active = true
+
+SGOpal.Name = ".AS.G Opal"
+SGOpal.Parent  = Main
+SGOpal.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SGOpal.BackgroundTransparency = 1.000
+SGOpal.Position = UDim2.new(-0.0199501235, 0, 0.272463769, 0)
+SGOpal.Size = UDim2.new(0, 200, 0, 50)
+SGOpal.Font = Enum.Font.SourceSans
+SGOpal.Text = "S.G Opal"
+SGOpal.TextColor3 = Color3.fromRGB(255, 255, 255)
+SGOpal.TextScaled = true
+SGOpal.TextSize = 14.000
+SGOpal.TextWrapped = true
+SGOpal.MouseButton1Click:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/uwQuezeF",true))()
+end)
+
+SG.Name = "S.G"
+SG.Parent = Main
+SG.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SG.BackgroundTransparency = 1.000
+SG.Position = UDim2.new(-0.0199501235, 0, 0.536231875, 0)
+SG.Size = UDim2.new(0, 200, 0, 50)
+SG.Font = Enum.Font.SourceSans
+SG.Text = "S.G"
+SG.TextColor3 = Color3.fromRGB(255, 255, 255)
+SG.TextScaled = true
+SG.TextSize = 14.000
+SG.TextWrapped = true
+SG.MouseButton1Click:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/YdsjmV1X",true))()
+end)
+
+SynapseX.Name = "Synapse X"
+SynapseX.Parent = Main
+SynapseX.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SynapseX.BackgroundTransparency = 1.000
+SynapseX.Position = UDim2.new(-0.0199501235, 0, 0.799999952, 0)
+SynapseX.Size = UDim2.new(0, 200, 0, 50)
+SynapseX.Font = Enum.Font.SourceSans
+SynapseX.Text = "Synapse X"
+SynapseX.TextColor3 = Color3.fromRGB(255, 255, 255)
+SynapseX.TextScaled = true
+SynapseX.TextSize = 14.000
+SynapseX.TextWrapped = true
+SynapseX.MouseButton1Click:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/6M1FqGzK",true))()
+end)
+
+ESploit.Name = "ESploit"
+ESploit.Parent = Main
+ESploit.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ESploit.BackgroundTransparency = 1.000
+ESploit.Position = UDim2.new(0.49875313, 0, 0.521739125, 0)
+ESploit.Size = UDim2.new(0, 200, 0, 50)
+ESploit.Font = Enum.Font.SourceSans
+ESploit.Text = "ESploit"
+ESploit.TextColor3 = Color3.fromRGB(255, 255, 255)
+ESploit.TextScaled = true
+ESploit.TextSize = 14.000
+ESploit.TextWrapped = true
+ESploit.MouseButton1Click:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/Y8k5sgMY",true))()
+end)
+
+TextLabel.Parent = Main
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(-0.0124688298, 0, -0.0202898532, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Fox X"
+TextLabel.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 67.000
+TextLabel.TextWrapped = true
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+Close.Name = "Close"
+Close.Parent = Main
+Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Close.Position = UDim2.new(0.835560799, 0, -0.0250115432, 0)
+Close.Size = UDim2.new(0, 71, 0, 50)
+Close.Style = Enum.ButtonStyle.RobloxRoundButton
+Close.Font = Enum.Font.SourceSans
+Close.Text = "X"
+Close.TextColor3 = Color3.fromRGB(255, 0, 0)
+Close.TextScaled = true
+Close.TextSize = 14.000
+Close.TextWrapped = true
+
+TextLabel_2.Parent = Main
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.241895258, 0, -0.0202898532, 0)
+TextLabel_2.Size = UDim2.new(0, 200, 0, 50)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "&"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 67.000
+TextLabel_2.TextWrapped = true
+TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_3.Parent = Main
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0.316708207, 0, -0.0231884029, 0)
+TextLabel_3.Size = UDim2.new(0, 200, 0, 50)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "LightRed"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 67.000
+TextLabel_3.TextWrapped = true
+TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
+
+RevzAdmin.Name = "Revz Admin"
+RevzAdmin.Parent = Main
+RevzAdmin.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RevzAdmin.BackgroundTransparency = 1.000
+RevzAdmin.Position = UDim2.new(0.483790547, 0, 0.272463769, 0)
+RevzAdmin.Size = UDim2.new(0, 200, 0, 50)
+RevzAdmin.Font = Enum.Font.SourceSans
+RevzAdmin.Text = "Revz Admin"
+RevzAdmin.TextColor3 = Color3.fromRGB(255, 255, 255)
+RevzAdmin.TextScaled = true
+RevzAdmin.TextSize = 14.000
+RevzAdmin.TextWrapped = true
+RevzAdmin.MouseButton1Click:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/ibFPdiF7",true))()
+end)
+
+TextButton.Parent = ScreenGui
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.Position = UDim2.new(0, 0, 0.528089881, 0)
+TextButton.Size = UDim2.new(0, 200, 0, 50)
+TextButton.Style = Enum.ButtonStyle.RobloxButton
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Open Fox X"
+TextButton.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+-- Scripts:
+
+local function XJLBR_fake_script() -- Close.LocalScript 
+	local script = Instance.new('LocalScript', Close)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		 script.Parent.Parent.Visible = false
+	end)
+end
+coroutine.wrap(XJLBR_fake_script)()
+local function LKKNZ_fake_script() -- Close.LocalScript 
+	local script = Instance.new('LocalScript', Close)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		 script.Parent.Parent.Visible = false
+	end)
+end
+coroutine.wrap(LKKNZ_fake_script)()
+local function AHPBDQ_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	local frame = script.Parent.Parent.Main
+	local open = false
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		if frame.Visible == false then
+			frame.Visible = true
+		else
+			frame.Visible = false
+		end
+	end)
+end
+coroutine.wrap(AHPBDQ_fake_script)()
